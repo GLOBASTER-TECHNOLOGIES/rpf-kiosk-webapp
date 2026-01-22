a469a689749403ad94ef7034538e89967b5135ad
Noel Sebu <noelsebu555@gmail.com>
2026-01-22 20:58:34 +0530

    method changed to post for a testing envoriment

--- app/api/lol/route.ts
+++ app/api/lol/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   return NextResponse.json(
     { message: "message successfully sent", success: true },
     { status: 200 }
